d28b41344396919defbde3aa5c38dd90fc636934
Jessica J <jesjar16@gmail.com>
2021-05-03 18:41:41 -0400

    Etapa 4: Se despliegan las recetas en Nuestras Recetas Favoritas

--- js/script-index.js
+++ js/script-index.js
@@ -3,7 +3,6 @@ $(document).ready( function(){
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
 	for (var i=0; i<recipesArray.length; i++) {
-		console.log(recipesArray.cookTime);
 		if (recipesArray[i].highlighted == true){
 			renderRecipe(recipesArray[i]);
 		}
@@ -26,7 +25,27 @@ function renderHighlightedRecipes(recipesArray) {
 * archivo "templates/templates-recipe.html"
 */
 function renderRecipe(recipe) {
+
+	var html_str= "";
+
 	console.log('Voy a pintar la receta: ', recipe);
+
+
+	html_str+= "<a class='item-recipe' href='#'>";
+	html_str+= "<span class='attribution'>";
+	html_str+= "<span class='title-recipe'>" + recipe.title + "</span>";
+	html_str+= "<span class='metadata-recipe'>";
+	html_str+= "<span class='author-recipe'>" + recipe.source.name + "</span>";
+	html_str+= "<span class='bookmarks-recipe'>";
+	html_str+= "<span class='icon-bookmark'></span>"; 
+	html_str+= "</span>";
+	html_str+= "</span>";
+	html_str+= "</span>";
+	html_str+= "<img src='img/recipes/640x800/" + recipe.name + ".jpg' /></a>";	
+	
+	$('.list-recipes').after(html_str);
+
+
 }
 
 
