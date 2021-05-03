7a3192044922e0ab6896ccd657ad278dbb7cff6f
Jessica J <jesjar16@gmail.com>
2021-05-03 16:55:19 -0400

    Etapa 3: Se modifica script-index.js para que seleccione recetas con valor highlighted en true y se llama a función renderRecipe

--- js/script-index.js
+++ js/script-index.js
@@ -2,7 +2,12 @@ $(document).ready( function(){
 
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
-
+	for (var i=0; i<recipesArray.length; i++) {
+		console.log(recipesArray.cookTime);
+		if (recipesArray[i].highlighted == true){
+			renderRecipe(recipesArray[i]);
+		}
+	}
 });
 
 
